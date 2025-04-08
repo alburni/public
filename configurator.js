6ae2eb33a0c6fcb151b52e4dae1ad7c73e9a1873
Benjamin Alburni <benjamin.alburni@gmail.com>
2025-04-08 12:17:55 +0200

    Create configurator.js

---
+++ configurator.js
@@ -0,0 +1,16 @@
+const message = "Hello world"
+const helloDiv = document.getElementById("configurator-container")
+let index = 0
+
+// Fonction pour afficher chaque lettre avec un délai
+function displayLetter() {
+  if (index < message.length) {
+    helloDiv.textContent += message.charAt(index)
+    index++
+    // Délai de 200 millisecondes entre chaque lettre
+    setTimeout(displayLetter, 200)
+  }
+  submit(message)
+}
+
+displayLetter()
